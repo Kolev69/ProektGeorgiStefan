14a100ccafc2a97a892eabff7bf82f71eedbb3e0
Kolev69 <120708761+Kolev69@users.noreply.github.com>
2023-06-21 08:29:25 +0300

    Add files via upload

---
+++ Project1/Model/ShoesType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Model
+{
+    public class ShoesType
+    {
+       public int Id { get; set; }
+       public ICollection<Shoes>Shoes { get; set; }
+
+    } 
+}
